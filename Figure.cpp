90f62b2c4293d9c94ec6783d3df4b7b218710f0e
Mikhaliev <pipko1910@gmail.com>
2024-04-21 13:18:52 +0300

    Figure

---
+++ Figure.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Figure.h"
+
+using namespace std;
+
+Figure::Figure(int x, int y, string color) {
+	this->x = x;
+	this->y = y;
+	this->color = color;
+}
+
+
---
+++ Figure.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Figure{
+public:
+	int x = 0;
+	int y = 0;
+	string color = "";
+
+	Figure(int x, int y, string color);
+
+};
